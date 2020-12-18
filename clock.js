f5bf3c8a8487de8b7ac3b0411c1906dbef0f6c4b
tsaiduzzaman <tasnim.s@nyu.edu>
2020-12-18 16:20:17 -0500

    Finished Clock

--- clock.js
+++ clock.js
@@ -1,17 +1,26 @@
 function Clock (){
     [this.hour, this.minute, this.second] = new Date().toLocaleTimeString("en-US").split(/:| /);
-    // setInterval(this._tick.bind(this), 1000);
+    setInterval(this._tick.bind(this), 1000);
 }
 
 Clock.prototype.printTime = function () {
-    debugger
+    // debugger
     console.log(`${this.hour}:${this.minute}:${this.second}`);
 }
 
 Clock.prototype._tick = function() {
-    debugger
-    let sec = parseInt(this.second, 10) + 1
-    this.second = `${sec}`;
-    
+    // debugger
+    if (this.minute === "60"){
+        let hour = (parseInt(this.hour, 10) + 1) % 12;
+        this.minute = '00';
+        this.hour = `${hour}`;
+    } else if (this.second === "60"){
+        let min = parseInt(this.minute, 10) + 1;
+        this.second = '00';
+        this.minute = `${min}`;
+    } else {
+        let sec = parseInt(this.second, 10) + 1;
+        this.second = `${sec}`;
+    }
     this.printTime();
 }
